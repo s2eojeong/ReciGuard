15fecd19398b6a66fc2f906499e46fcfc54dba24
s2eojeong <s2eojeong@gmail.com>
2024-11-27 00:07:52 +0900

    initial setting

---
+++ src/main/java/com/ReciGuard/ReciGuardApplication.java
@@ -0,0 +1,13 @@
+package com.ReciGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ReciGuard/ReciGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReciGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
