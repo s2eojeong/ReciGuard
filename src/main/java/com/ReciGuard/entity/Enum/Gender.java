f4b4f12d353fb09c98edba5755ad7e520448e980
Handongbeom <zzx010319@naver.com>
2024-11-30 17:12:25 +0900

    Update: Gender

--- src/main/java/com/ReciGuard/entity/Enum/Gender.java
+++ src/main/java/com/ReciGuard/entity/Enum/Gender.java
@@ -1,5 +1,18 @@
 package com.ReciGuard.entity.Enum;
 
 public enum Gender {
-    man("남성"), woman(여성)
+    MAN("남자"),
+    WOMAN("여자");
+
+    private final String description;
+
+    // 생성자
+    Gender(String description) {
+        this.description = description;
+    }
+
+    // Getter 메서드
+    public String getDescription() {
+        return description;
+    }
 }
