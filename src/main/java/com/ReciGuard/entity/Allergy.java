06da5ebb249d77135760ff7915d5583a4c8178d3
s2eojeong <s2eojeong@gmail.com>
2024-11-30 17:13:46 +0900

    ERD 수정에 따른 Entity 수정

--- src/main/java/com/ReciGuard/entity/Allergy.java
+++ src/main/java/com/ReciGuard/entity/Allergy.java
@@ -2,12 +2,14 @@ package com.ReciGuard.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Allergy {
     @Id @GeneratedValue
     @Column(name = "allergy_id")
--- src/main/java/com/ReciGuard/entity/Ingredient.java
+++ src/main/java/com/ReciGuard/entity/Ingredient.java
@@ -2,10 +2,12 @@ package com.ReciGuard.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Ingredient {
     @Id @GeneratedValue
     @Column(name = "ingredient_id")
