30441188b11a9d25113b5a7dab342789f197d17e
s2eojeong <s2eojeong@gmail.com>
2024-12-21 15:59:30 +0900

    Response DTO 추가

--- src/main/java/com/ReciGuard/dto/RecipeListResponseDTO.java
+++ src/main/java/com/ReciGuard/dto/RecipeListResponseDTO.java
@@ -1,13 +1,20 @@
 package com.ReciGuard.dto;
 
+import com.ReciGuard.entity.Recipe;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter @Setter
-public class RecipeListResponseDTO {
+public class RecipeListResponseDTO { //전체 리스트, cuisine 별 리스트, 검색 후 리스트
     private String imagePath;
     private String recipeName;
     private int serving;
+
+    public RecipeListResponseDTO(String recipeName, String imagePath, int serving) {
+        this.recipeName = recipeName;
+        this.imagePath = imagePath;
+        this.serving = serving;
+    }
 }
+
+
