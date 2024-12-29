ed5a2f2df0c39d47070f038e70276362a7cdb4bc
Kim Minju <rlaalswb0523@naver.com>
2024-12-30 01:05:39 +0900

    [logo]

--- Reciguard/src/components/Header2.jsx
+++ Reciguard/src/components/Header2.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Header2.css";
 import mypage from '../assets/마이페이지.png'
+import { Link } from 'react-router-dom';
 
 const Header2 = () => {
     return (
     <nav className="navbar2">
+        <Link className="logolink" to="/Realmain">
         <div className="navbar-logo2">ReciGuard</div>
+        </Link>
         <ul className="navbar-menu2">
         <li>분류</li>
         <li>랭킹</li>
