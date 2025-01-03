689d81cf15cee64648810c2d0fec4378f55b1263
s2eojeong <s2eojeong@gmail.com>
2025-01-03 17:37:02 +0900

    폼 수정

--- src/main/java/com/ReciGuard/dto/MyRecipeForm.java
+++ src/main/java/com/ReciGuard/dto/MyRecipeForm.java
@@ -8,11 +8,15 @@ import java.util.List;
 @Getter @Setter
 public class MyRecipeForm {
 
-    private Long id;
+    private String recipeName;                       // 레시피 이름
+    private String imagePath;                        // 이미지 경로(메인 이미지)
+    private String userId;                           // 작성자 ID
+    private int serving;                             // 인분 수
+    private String cuisine;                          // 요리 종류 (예: 한식, 양식 등)
+    private String foodType;                         // 음식 타입 (예: 밥, 면, 빵 등)
+    private String cookingStyle;                     // 조리 방식 (예: 볶음, 찜 등)
 
-    private String recipeName;
-    private int cookingTime;
-    private String cuisine;
-    private List<IngredientResponseDTO> ingredients;
+    private List<IngredientResponseDTO> ingredients; // 재료 정보
+    private List<InstructionResponseDTO> instructions; // 조리 단계 정보
 
 }
