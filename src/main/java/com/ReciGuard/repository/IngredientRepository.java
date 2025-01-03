e63d3456ff8c3234b47697bbeee45849544307b9
s2eojeong <s2eojeong@gmail.com>
2025-01-03 17:36:48 +0900

    repository 수정

---
+++ src/main/java/com/ReciGuard/repository/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.ReciGuard.repository;
+
+import com.ReciGuard.entity.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+
+    Ingredient findByName(String name);
+}
