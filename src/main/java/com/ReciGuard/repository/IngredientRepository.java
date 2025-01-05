f32e72b9950d5b067c878416263dd03ad62f5b2e
s2eojeong <s2eojeong@gmail.com>
2025-01-05 16:14:38 +0900

    error 수정

--- src/main/java/com/ReciGuard/repository/IngredientRepository.java
+++ src/main/java/com/ReciGuard/repository/IngredientRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-
-    Ingredient findByName(String name);
+    Ingredient findByIngredient(String ingredient);
 }
