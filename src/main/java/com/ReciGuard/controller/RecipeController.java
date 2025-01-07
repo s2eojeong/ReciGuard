f50c724bbe42cbc2cb6965e07b082ae3c3659285
s2eojeong <s2eojeong@gmail.com>
2025-01-07 11:47:11 +0900

    recipe 조회 코드 간편화

--- src/main/java/com/ReciGuard/controller/RecipeController.java
+++ src/main/java/com/ReciGuard/controller/RecipeController.java
@@ -8,15 +8,12 @@ import com.ReciGuard.service.RecipeStatsService;
 import com.ReciGuard.service.UserScrapService;
 import com.ReciGuard.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/recipes")
@@ -35,47 +32,39 @@ public class RecipeController {
 
     // 전체 레시피 리스트
     @GetMapping("/all")
-    public List<RecipeListResponseDTO> getAllRecipes(){
+    public List<RecipeListResponseDTO> getRecipes(@RequestParam(required = false, defaultValue = "false") boolean filter) {
+        if (filter) {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            Long userId = userService.findUserIdByUsername(username);
+            return recipeService.getAllFilteredRecipes(userId);
+        }
         return recipeService.getAllRecipes();
-    }
-
-    // 전체 레시피 리스트 (사용자 알레르기 정보 바탕으로 필터링 후)
-    @GetMapping("/all/filtered")
-    public List<RecipeListResponseDTO> getAllFilteredRecipes(){
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        Long userId = userService.findUserIdByUsername(username);
-
-        return recipeService.getAllFilteredRecipes(userId);
     }
 
     // cuisine별 레시피 리스트
     @GetMapping
-    public List<RecipeListResponseDTO> getRecipesByCuisine(@RequestParam String cuisine){
+    public List<RecipeListResponseDTO> getRecipesByCuisine(
+            @RequestParam String cuisine,
+            @RequestParam(required = false, defaultValue = "false") boolean filter) {
+        if (filter) {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            Long userId = userService.findUserIdByUsername(username);
+            return recipeService.getFilteredRecipesByCuisine(userId, cuisine);
+        }
         return recipeService.getRecipesByCuisine(cuisine);
-    }
-
-    // cuisine별 레시피 리스트 (사용자 알레르기 정보 바탕으로 필터링 후)
-    @GetMapping("/filtered")
-    public List<RecipeListResponseDTO> getRecipesFilteredByCuisine(@RequestParam String cuisine){
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        Long userId = userService.findUserIdByUsername(username);
-
-        return recipeService.getFilteredRecipesByCuisine(userId, cuisine);
     }
 
     // query로 레시피 검색
     @GetMapping("/search")
-    public List<RecipeListResponseDTO> getRecipesByQuery(@RequestParam String query){
+    public List<RecipeListResponseDTO> getRecipesByQuery(
+            @RequestParam String query,
+            @RequestParam(required = false, defaultValue = "false") boolean filter) {
+        if (filter) {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            Long userId = userService.findUserIdByUsername(username);
+            return recipeService.getFilteredRecipesByQuery(userId, query);
+        }
         return recipeService.getRecipesByQuery(query);
-    }
-
-    // query로 레시피 검색 (사용자 알레르기 정보 바탕으로 필터링 후)
-    @GetMapping("search/filtered")
-    public List<RecipeListResponseDTO> getRecipesFilteredByQuery(@RequestParam String query){
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        Long userId = userService.findUserIdByUsername(username);
-
-        return recipeService.getFilteredRecipesByQuery(userId, query);
     }
 
     // 레시피 상세 페이지
