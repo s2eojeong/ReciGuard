0a5411ed806433530210877f46fca6ea7f46ee64
Kim Minju <rlaalswb0523@naver.com>
2025-01-11 14:58:29 +0900

    [마이페이지홈 수정중]

--- Reciguard/src/pages/Mypage/MypageHome.jsx
+++ Reciguard/src/pages/Mypage/MypageHome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Header2 from "../components/Header2";
-import Footer from "../components/Footer";
-import Sidebar1 from "../components/Mypage/Sidebar1";
+import Header2 from "../../components/Header2";
+import Footer from "../../components/Footer";
+import Sidebar1 from "../../components/Mypage/Sidebar1";
+import HomeSide from "../../components/Mypage/HomeSide";
 
 function MypageHome() {
   return (
@@ -10,6 +11,7 @@ function MypageHome() {
         <Header2 />
       </div>
       <Sidebar1 />
+      <HomeSide />
       <Footer />
     </div>
   );
