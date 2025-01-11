fdd2460cb30046f2369c41630bcfbaea91caea17
Handongbeom <zzx010319@naver.com>
2025-01-11 15:52:48 +0900

    jwt 토큰 body에 포함

--- src/main/java/com/ReciGuard/controller/AuthController.java
+++ src/main/java/com/ReciGuard/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.ReciGuard.controller;
 
+import com.ReciGuard.JWT.JWTUtil;
 import com.ReciGuard.dto.UserResponseDTO;
 import com.ReciGuard.service.UserService;
 import jakarta.validation.Valid;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
+    private final JWTUtil jwtUtil;
 
 
     // 회원가입
@@ -44,8 +48,12 @@ public class AuthController {
         }
             UserResponseDTO.Request loginResult = userService.login(userDTO);
             if (loginResult != null) {
-                // 로그인 성공 시 메시지 반환
-                return ResponseEntity.ok().body("로그인에 성공했습니다.");
+                String token = jwtUtil.createJwt(userDTO.getEmail(), "ROLE_USER", 60 * 60 * 1000L); // 토큰 생성
+                Map<String, String> responseBody = Map.of(
+                        "message", "로그인 성공",
+                        "token", token
+                );
+                return ResponseEntity.ok(responseBody);
             } else {
                 // 로그인 실패 시 메시지 반환
                 return ResponseEntity.status(401).body("로그인에 실패했습니다.");
