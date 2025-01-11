543784a0ae47392f22ab9d97347361e044ee7928
Kim Minju <rlaalswb0523@naver.com>
2025-01-11 15:59:31 +0900

    [로그인 백 머지]

--- Reciguard/src/components/LoginSignup/Rightpanel.jsx
+++ Reciguard/src/components/LoginSignup/Rightpanel.jsx
@@ -12,7 +12,7 @@ function Rightpanel() {
     e.preventDefault(); // 기본 폼 동작 방지
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/login", {
+      const response = await fetch("http://localhost:8080/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
