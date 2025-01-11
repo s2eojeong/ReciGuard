d06dde377261f2e73a8dbdd0ba95279e8e249dd4
s2eojeong <s2eojeong@gmail.com>
2025-01-11 16:01:55 +0900

    data 수정

--- src/main/java/com/ReciGuard/entity/Nutrition.java
+++ src/main/java/com/ReciGuard/entity/Nutrition.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor
 public class Nutrition {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "nutrition_id")
     private Long id;
 
