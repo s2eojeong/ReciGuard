fe9d5781b9ad5c029c0ea065bb203585fc952541
Kim Minju <rlaalswb0523@naver.com>
2025-01-11 15:59:31 +0900

    [로그인 백 머지]

--- src/main/java/com/ReciGuard/controller/AuthController.java
+++ src/main/java/com/ReciGuard/controller/AuthController.java
@@ -1,17 +1,22 @@
 package com.ReciGuard.controller;
 
-import com.ReciGuard.JWT.JWTUtil;
-import com.ReciGuard.dto.UserResponseDTO;
-import com.ReciGuard.service.UserService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.ReciGuard.JWT.JWTUtil;
+import com.ReciGuard.dto.UserResponseDTO;
+import com.ReciGuard.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -21,7 +26,6 @@ public class AuthController {
 
     private final UserService userService;
     private final JWTUtil jwtUtil;
-
 
     // 회원가입
     @PostMapping("/register")
@@ -46,30 +50,28 @@ public class AuthController {
         if (userDTO.getEmail() == null || userDTO.getEmail().isEmpty()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이메일을 입력해주세요.");
         }
-            UserResponseDTO.Request loginResult = userService.login(userDTO);
-            if (loginResult != null) {
-                String token = jwtUtil.createJwt(userDTO.getEmail(), "ROLE_USER", 60 * 60 * 1000L); // 토큰 생성
-                Map<String, String> responseBody = Map.of(
-                        "message", "로그인 성공",
-                        "token", token
-                );
-                return ResponseEntity.ok(responseBody);
-            } else {
-                // 로그인 실패 시 메시지 반환
-                return ResponseEntity.status(401).body("로그인에 실패했습니다.");
-            }
-        }
-
-        // 비밀번호 변경
-        @PostMapping("/password")
-        public ResponseEntity<?> changePasswordByEmail (@RequestBody UserResponseDTO.Request userDTO){
-            try {
-                userService.modify(userDTO);
-                return ResponseEntity.ok().body("비밀번호가 성공적으로 변경되었습니다.");
-            } catch (IllegalArgumentException e) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-            }
+        UserResponseDTO.Request loginResult = userService.login(userDTO);
+        if (loginResult != null) {
+            String token = jwtUtil.createJwt(userDTO.getEmail(), "ROLE_USER", 60 * 60 * 1000L); // 토큰 생성
+            Map<String, String> responseBody = Map.of(
+                    "message", "로그인 성공",
+                    "token", token
+            );
+            return ResponseEntity.ok(responseBody);
+        } else {
+            // 로그인 실패 시 메시지 반환
+            return ResponseEntity.status(401).body("로그인에 실패했습니다.");
         }
     }
 
-
+    // 비밀번호 변경
+    @PostMapping("/password")
+    public ResponseEntity<?> changePasswordByEmail(@RequestBody UserResponseDTO.Request userDTO) {
+        try {
+            userService.modify(userDTO);
+            return ResponseEntity.ok().body("비밀번호가 성공적으로 변경되었습니다.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+}
