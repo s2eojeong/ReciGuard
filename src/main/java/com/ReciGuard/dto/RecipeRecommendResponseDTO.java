08fb09592f2905809ac5d76d522229e09d8a454a
s2eojeong <s2eojeong@gmail.com>
2025-01-15 16:24:44 +0900

    todayRecipe 응답값에 recipeId 추가

--- src/main/java/com/ReciGuard/dto/RecipeRecommendResponseDTO.java
+++ src/main/java/com/ReciGuard/dto/RecipeRecommendResponseDTO.java
@@ -7,10 +7,12 @@ import lombok.Setter;
 
 @Getter @Setter
 public class RecipeRecommendResponseDTO { // 오늘의 추천 레시피
+    private Long recipeId;
     private String imagePath;
     private String recipeName;
 
-    public RecipeRecommendResponseDTO(String imagePath, String recipeName) {
+    public RecipeRecommendResponseDTO(Long recipeId, String imagePath, String recipeName) {
+        this.recipeId = recipeId;
         this.imagePath = imagePath;
         this.recipeName = recipeName;
     }
