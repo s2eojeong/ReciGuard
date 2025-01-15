99b3407ef9ebef771d6194a06256cb459c520d8b
s2eojeong <s2eojeong@gmail.com>
2025-01-15 16:00:21 +0900

    응답값에 recipeId 추가

--- src/main/java/com/ReciGuard/dto/RecipeListResponseDTO.java
+++ src/main/java/com/ReciGuard/dto/RecipeListResponseDTO.java
@@ -6,11 +6,13 @@ import lombok.Setter;
 
 @Getter @Setter
 public class RecipeListResponseDTO { //전체 리스트, cuisine 별 리스트, 검색 후 리스트
+    private Long recipeId;
     private String imagePath;
     private String recipeName;
     private int serving;
 
-    public RecipeListResponseDTO(String recipeName, String imagePath, int serving) {
+    public RecipeListResponseDTO(Long recipeId, String recipeName, String imagePath, int serving) {
+        this.recipeId = recipeId;
         this.recipeName = recipeName;
         this.imagePath = imagePath;
         this.serving = serving;
