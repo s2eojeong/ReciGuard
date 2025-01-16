39df75b1eb04aab77a46940bf9c90eccfe2053ea
Kim Minju <rlaalswb0523@naver.com>
2025-01-16 16:34:09 +0900

    merge 충돌 해결

--- src/main/java/com/ReciGuard/repository/UserFoodTypeRepository.java
+++ src/main/java/com/ReciGuard/repository/UserFoodTypeRepository.java
@@ -1,5 +1,6 @@
 package com.ReciGuard.repository;
 
+import com.ReciGuard.entity.UserCookingStyle;
 import com.ReciGuard.entity.UserFoodType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -7,9 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface UserFoodTypeRepository extends JpaRepository<UserFoodType,Long> {
     @Modifying
     @Transactional
     @Query("DELETE FROM UserFoodType uft WHERE uft.user.id = :userId")
     void deleteByUserId(@Param("userId")Long userid);
+
+    @Query("SELECT uft FROM UserFoodType uft WHERE uft.user.id = :userId")
+    List<UserFoodType> findByUserId(@Param("userId") Long userId);
 }
