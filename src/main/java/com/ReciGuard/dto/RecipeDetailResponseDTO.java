2cfd07781d59581f1099ab1168b9dfcd97f3f5c3
s2eojeong <s2eojeong@gmail.com>
2025-01-16 20:09:27 +0900

    ai 모델 파트 수정중

--- src/main/java/com/ReciGuard/dto/RecipeDetailResponseDTO.java
+++ src/main/java/com/ReciGuard/dto/RecipeDetailResponseDTO.java
@@ -25,6 +25,7 @@ public class RecipeDetailResponseDTO {
 
     private List<IngredientResponseDTO> ingredients; // 재료 리스트
     private List<InstructionResponseDTO> instructions; // 조리 과정 리스트
+    private List<String> similarAllergyIngredients; // 유사 알레르기 유발 재료
 
     public RecipeDetailResponseDTO(String imagePath,
                                    String recipeName,
@@ -35,7 +36,8 @@ public class RecipeDetailResponseDTO {
                                    int calories, int sodium, int carbohydrate, int fat, int protein,
                                    int scrapCount, int viewCount,
                                    List<IngredientResponseDTO> ingredients,
-                                   List<InstructionResponseDTO> instructions) {
+                                   List<InstructionResponseDTO> instructions,
+                                   List<String> similarAllergyIngredients) {
         this.imagePath = imagePath;
         this.recipeName = recipeName;
         this.serving = serving;
@@ -51,6 +53,7 @@ public class RecipeDetailResponseDTO {
         this.viewCount = viewCount;
         this.ingredients = ingredients;
         this.instructions = instructions;
+        this.similarAllergyIngredients = similarAllergyIngredients;
     }
 
 }
