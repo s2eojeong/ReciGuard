f02b82304436dc81248e739ccc805aae50dde5ce
s2eojeong <s2eojeong@gmail.com>
2025-01-18 21:27:35 +0900

    AWS S3 연동 & 이미지 업로드 구현

--- src/main/java/com/ReciGuard/repository/InstructionRepository.java
+++ src/main/java/com/ReciGuard/repository/InstructionRepository.java
@@ -2,6 +2,13 @@ package com.ReciGuard.repository;
 
 import com.ReciGuard.entity.Instruction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface InstructionRepository extends JpaRepository<Instruction, Long> {
+    @Query("SELECT MAX(i.instructionId) FROM Instruction i WHERE i.recipe.id = :recipeId")
+    Optional<Integer> findMaxInstructionIdByRecipeId(@Param("recipeId") Long recipeId);
+
 }
