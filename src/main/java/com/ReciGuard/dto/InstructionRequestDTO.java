668c73019a7bd337e3f52d8f9c45659db05c27a9
s2eojeong <s2eojeong@gmail.com>
2025-01-19 00:56:49 +0900

    updateMyRecipe 수정

--- src/main/java/com/ReciGuard/dto/InstructionRequestDTO.java
+++ src/main/java/com/ReciGuard/dto/InstructionRequestDTO.java
@@ -12,16 +12,20 @@ public class InstructionRequestDTO {
     private Integer instructionId;
     private MultipartFile instructionImageFile;
     private String instruction;
-    private boolean imageRemoved;
+    private boolean ImageRemoved;
     private String instructionImage; // 추가: S3에 업로드된 이미지 URL 저장
 
     @JsonCreator
-    public InstructionRequestDTO(@JsonProperty("instruction") String instruction,
+    public InstructionRequestDTO(@JsonProperty("instructionId") Integer instructionId,
+                                 @JsonProperty("instruction") String instruction,
                                  @JsonProperty("imageRemoved") boolean imageRemoved,
-                                 @JsonProperty("instructionImageFile") MultipartFile instructionImageFile) {
+                                 @JsonProperty("instructionImageFile") MultipartFile instructionImageFile,
+                                 @JsonProperty("instructionImage") String instructionImage) {
+        this.instructionId = instructionId;
         this.instruction = instruction;
-        this.imageRemoved = imageRemoved;
+        this.ImageRemoved = imageRemoved;
         this.instructionImageFile = instructionImageFile;
+        this.instructionImage = instructionImage;
     }
 
     public boolean hasInstructionImage() {
