86b6f68ba7f0a4627c00bdb808862b1836869f01
Kim Minju <rlaalswb0523@naver.com>
2025-01-18 17:08:53 +0900

    검색 완료

---
+++ Reciguard/src/pages/SearchPage.jsx
@@ -0,0 +1,15 @@
+import Header2 from "../components/Header2";
+import Footer from "../components/Footer";
+import Search from "../components/AllRecipe/Search.jsx";
+
+const AllRecipes = () => {
+    return (
+        <div>
+            <Header2 />
+            <Search />
+            <Footer />
+        </div>
+    );
+};
+
+export default AllRecipes;
