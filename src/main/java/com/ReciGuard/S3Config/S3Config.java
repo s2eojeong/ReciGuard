dc97432fb17b8a6d772b82b2a0a1a4b740389484
s2eojeong <s2eojeong@gmail.com>
2025-01-18 13:52:07 +0900

    .yml .gitignore 에 추가

--- src/main/java/com/ReciGuard/S3Config/S3Config.java
+++ src/main/java/com/ReciGuard/S3Config/S3Config.java
@@ -1,2 +1,23 @@
-package com.ReciGuard.S3Config;public class S3Config {
+package com.ReciGuard.S3Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+//    @Bean
+//    public AmazonS3Client amazonS3Client(){
+//
+//    }
 }
