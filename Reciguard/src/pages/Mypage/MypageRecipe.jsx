257e8eb29e675eebd342530e368796238b12c6bd
Kim Minju <rlaalswb0523@naver.com>
2025-01-19 20:00:45 +0900

    [마이페이지에서 나만의 레시피 불러오기 완료]

--- Reciguard/src/pages/Mypage/MypageRecipe.jsx
+++ Reciguard/src/pages/Mypage/MypageRecipe.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import Header2 from "../../components/Header2";
 import Footer from "../../components/Footer";
 import Sidebar3 from "../../components/Mypage/Sidebar3";
+import MypageMyRecipe from "../../components/Mypage/MypageMyRecipe.jsx";
 
 function MypageRecipe() {
   return (
-    <div>
-      <div style={{ borderBottom: "2px solid #ddd" }}>
-        <Header2 />
+      <div>
+          <div style={{borderBottom: "2px solid #ddd"}}>
+              <Header2/>
+          </div>
+          <div className="MypageHome-Container">
+              <Sidebar3/>
+              <MypageMyRecipe/>
+          </div>
+          <Footer/>
       </div>
-      <Sidebar3 />
-      <Footer />
-    </div>
   );
 }
 
