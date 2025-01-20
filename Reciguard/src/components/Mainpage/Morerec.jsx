cd9c4eddaf4e99f2ad88cda34427a24baf05c61b
Kim Minju <rlaalswb0523@naver.com>
2025-01-20 20:35:18 +0900

    [선호도 수정 오류 해결]

--- Reciguard/src/components/Mainpage/Morerec.jsx
+++ Reciguard/src/components/Mainpage/Morerec.jsx
@@ -1,32 +1,49 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Morerec.css";
+import 한식 from "../../assets/한식 대표 사진.jpg";
+import 중식 from "../../assets/중식 대표 사진.png";
+import 일식 from "../../assets/일식 대표 사진.png";
+import 양식 from "../../assets/양식 대표 사진.png";
+import 아시안 from "../../assets/아시안 대표 사진.jpg";
 
 const Morerec = () => {
-  const categories = [
-    { name: "한식", image: "placeholder.jpg" },
-    { name: "중식", image: "placeholder.jpg" },
-    { name: "일식", image: "placeholder.jpg" },
-    { name: "양식", image: "placeholder.jpg" },
-    { name: "아시안", image: "placeholder.jpg" },
-  ];
+    const navigate = useNavigate();
 
-  return (
-    <div className="more-recipes">
-      <h2 className="more-recipes-title">더 많은 레시피</h2>
-      <div className="recipe-category-container">
-        {categories.map((category, index) => (
-          <div className="recipe-category" key={index}>
-            <img
-              src={category.image}
-              alt={category.name}
-              className="recipe-image"
-            />
-            <p className="recipe-name">{category.name}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    const categories = [
+        { name: "한식", image: 한식 },
+        { name: "중식", image: 중식 },
+        { name: "일식", image: 일식 },
+        { name: "양식", image: 양식 },
+        { name: "아시안", image: 아시안 },
+    ];
+
+    const handleCategoryClick = (category) => {
+        // 쿼리 파라미터로 카테고리 전달
+        navigate(`/recipes/all`);
+    };
+
+    return (
+        <div className="more-recipes">
+            <h2 className="more-recipes-title">더 많은 레시피</h2>
+            <div className="recipe-category-container">
+                {categories.map((category, index) => (
+                    <div
+                        className="recipe-category"
+                        key={index}
+                        onClick={() => handleCategoryClick(category.name)} // 클릭 이벤트 추가
+                    >
+                        <img
+                            src={category.image}
+                            alt={category.name}
+                            className="recipe-image"
+                        />
+                        <p className="recipe-name">{category.name}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Morerec;
