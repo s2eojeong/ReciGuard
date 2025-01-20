d46d0a92ca63b1fc9287cc1d1dc4035713dc0fde
Kim Minju <rlaalswb0523@naver.com>
2025-01-21 02:17:28 +0900

    [프론트는 배포중]

--- Reciguard/src/components/Rdetail.jsx
+++ Reciguard/src/components/Rdetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./Rdetail.css";
-import 몇인분 from "../assets/serving";
+import 몇인분 from "../assets/serving.png";
 import 국가별 from "../assets/cuisine.png";
 import 조회수 from "../assets/count.png";
 import filledHeartImg from "../assets/heart1.png";
