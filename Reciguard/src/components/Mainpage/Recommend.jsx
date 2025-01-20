784a8283b659efc1ba60230b9be171ba4fd5597b
Kim Minju <rlaalswb0523@naver.com>
2025-01-21 08:53:49 +0900

    [cors, api 재설정]

--- Reciguard/src/components/Mainpage/Recommend.jsx
+++ Reciguard/src/components/Mainpage/Recommend.jsx
@@ -14,7 +14,7 @@ const Recommend = () => {
     const fetchRecommendRecipe = async () => {
       try {
         const response = await fetch(
-          `https://reciguard.comhttps://reciguard.com/recipes/recommend`,
+          `https://reciguard.com/recipes/recommend`,
           {
             method: "GET",
             headers: {
