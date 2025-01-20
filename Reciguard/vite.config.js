4af06184026bb7b0b173d26e4b955f449b29fdce
Kim Minju <rlaalswb0523@naver.com>
2025-01-21 02:17:28 +0900

    [프론트는 배포중]

--- Reciguard/vite.config.js
+++ Reciguard/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 상대 경로 설정
+  build: {
+    outDir: 'dist', // 빌드 결과물이 저장될 디렉토리
+    assetsDir: 'assets', // 정적 파일이 저장될 디렉토리
+  },
 })
