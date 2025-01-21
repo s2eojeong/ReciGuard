c1f2a39c1615a02a1f109415af9740c651b286ac
Kim Minju <rlaalswb0523@naver.com>
2025-01-21 11:55:41 +0900

    [css 비율 조정]

--- src/main/java/com/ReciGuard/webconfig/Webconfig.java
+++ src/main/java/com/ReciGuard/webconfig/Webconfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Webconfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 URL 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:5173") // 프론트엔드 주소 허용
+                .allowedOrigins("https://www.reciguard.com") // 프론트엔드 주소 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 인증 허용
