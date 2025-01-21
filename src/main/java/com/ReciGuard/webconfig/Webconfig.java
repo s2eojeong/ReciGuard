df7e0a01630da3047261bbdfd8dc4cb817b9869f
Handongbeom <zzx010319@naver.com>
2025-01-21 13:35:55 +0900

    .

--- src/main/java/com/ReciGuard/webconfig/Webconfig.java
+++ src/main/java/com/ReciGuard/webconfig/Webconfig.java
@@ -10,7 +10,7 @@ public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 URL 경로에 대해 CORS 허용
-                .allowedOrigins("https://www.reciguard.com") // 프론트엔드 주소 허용
+                .allowedOrigins("https://reciguard.com","https://www.reciguard.com") // 프론트엔드 주소 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 인증 허용
