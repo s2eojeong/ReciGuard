f9696050f1a0870f406591b9f3f4bf5cb5e6e869
Handongbeom <zzx010319@naver.com>
2025-01-21 13:59:06 +0900

    .

--- src/main/java/com/ReciGuard/controller/RecipeController.java
+++ src/main/java/com/ReciGuard/controller/RecipeController.java
@@ -35,7 +35,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("//apirecipes")
+@RequestMapping("/api/recipes")
 public class RecipeController {
 
     private final RecipeService recipeService;
